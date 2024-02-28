3ab0891db63e7e0cb59b54553542a46ff92ef5dd
Albin <Overkord123@hotmail.com>
2024-02-28 16:55:00 +0100

    Add project files.

---
+++ WebAppMVC/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppMVC.Controllers;
+
+public class HomeController : Controller
+{
+
+    // https://domain.com/Index
+    public IActionResult Index()
+    {
+        ViewData["Title"] = "Ultimate Task Management Assistant";
+
+        return View();
+
+    }
+}
