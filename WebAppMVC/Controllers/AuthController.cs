5f7a70e92ed0e64ab1709043e983cd90521e2ad6
Albin <Overkord123@hotmail.com>
2024-02-29 12:20:58 +0100

    middle off scss to signup form

--- WebAppMVC/Controllers/AuthController.cs
+++ WebAppMVC/Controllers/AuthController.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebAppMVC.ViewModels;
 
 namespace WebAppMVC.Controllers;
 
 public class AuthController : Controller
 {
     [Route("/signup")]
+    [HttpGet]
     public IActionResult SignUp()
     {
-        return View();
+        var viewModel = new SignUpViewModel();
+        return View(viewModel);
+    }
+
+    [Route("/signup")]
+    [HttpPost]
+    public IActionResult SignUp(SignUpViewModel viewModel)
+    {
+        if (!ModelState.IsValid)
+            return View(viewModel);
+
+        return RedirectToAction("SignIn", "Auth");
     }
 }
