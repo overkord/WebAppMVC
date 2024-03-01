09773ef93f80fcf493fa99ac3506061ec8f8127c
Albin <Overkord123@hotmail.com>
2024-03-01 15:15:46 +0100

    account init

--- WebAppMVC/Controllers/AccountController.cs
+++ WebAppMVC/Controllers/AccountController.cs
@@ -5,5 +5,11 @@ namespace WebAppMVC.Controllers;
     public class AccountController : Controller
     {
 
+    //private readonly AccountService _ accountService;
+
+    //    public AccountController(AccountService accountService)
+    //{
+    //    _accountService = accountService;
+    //}
     }
 
