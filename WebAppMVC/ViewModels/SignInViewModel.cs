95a340820eefccc4288d2a04619010f2ae69db65
Albin <Overkord123@hotmail.com>
2024-03-01 14:21:42 +0100

    SignIn not really completed, scss is fucked up

---
+++ WebAppMVC/ViewModels/SignInViewModel.cs
@@ -0,0 +1,11 @@
+﻿using WebAppMVC.Models;
+
+namespace WebAppMVC.ViewModels;
+
+public class SignInViewModel
+{
+    public string Title { get; set; } = "Sign In";
+    public SignInModel Form { get; set; } = new SignInModel();
+    public string? ErrorMessaage { get; set; }
+
+}
